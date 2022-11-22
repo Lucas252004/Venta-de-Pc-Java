db5f4de23abb32ff01e0b1bc251e44e451614dc6
Lucas252004 <lucas.bermudez135@gmail.com>
2022-11-21 22:15:21 -0300

    Base de datos

---
+++ db/bd_venta_pc_gestion.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bd_venta_pc
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gestion`
+--
+
+DROP TABLE IF EXISTS `gestion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gestion` (
+  `idGestion` int NOT NULL AUTO_INCREMENT,
+  `fondos` int DEFAULT NULL,
+  `producto_vendido` varchar(55) DEFAULT NULL,
+  `ingresos` int DEFAULT NULL,
+  `gastos_comiciones` int DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`idGestion`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gestion`
+--
+
+LOCK TABLES `gestion` WRITE;
+/*!40000 ALTER TABLE `gestion` DISABLE KEYS */;
+INSERT INTO `gestion` VALUES (1,1,'test',1,1,'2021-11-16'),(2,2,'test',1,1,'2021-11-16'),(3,2,'test',1,1,'2021-11-16'),(4,NULL,'test',NULL,NULL,'2021-11-16'),(8,NULL,'test',NULL,NULL,'2021-11-16'),(9,NULL,'PC1',NULL,NULL,'2021-11-16'),(10,NULL,'PC2',NULL,NULL,'2021-11-16'),(11,NULL,'PC1',NULL,NULL,'2021-11-02'),(12,NULL,'PC1',NULL,NULL,'2021-11-02'),(13,NULL,'PC2',NULL,NULL,'2021-11-02');
+/*!40000 ALTER TABLE `gestion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-23 11:29:16
